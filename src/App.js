1c64fb1bb7c90f573223d0082800dca4ba910378
jappu2 <jaber.fallatah@gmail.com>
2023-02-13 06:27:20 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,115 @@
-import logo from './logo.svg';
+import {useState} from 'react'
+import axios from 'axios'
 import './App.css';
 
+
+
 function App() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordRe, setPasswordRe] = useState('')
+
+  function submit(e){
+    e.preventDefault();
+    if (allClear()) { 
+      // Send Data
+      console.log('all clear.. data sent!')
+      let res = axios.post('http://127.0.0.1:8000/api/regester', {
+        name: name,
+        email: email,
+        password: password,
+        conferm_password: passwordRe
+      }).then(res=>console.log(res))
+      
+    } else{
+
+    }
+  }
+
+  function allClear(){
+    return !nameError() && !emailError() && !passwordReError() && !passwordError()
+  }
+
+  function nameError(){
+    if (!name) return 'Please enter a name!'
+    if (name.split(' ').length > 3) return 'a name can\'t be more than 3 words!'
+    if (name.split('').filter(e => e == '0' || +e >= 1 && +e <= 9).length >= 1) return 'a name can\'t contain numbers'
+    return false
+  }
+
+  function emailError(){
+    if (!email) return 'Please provide a proper email!'
+    return false
+  }
+
+  function passwordError(){
+    if (password.length < 8) return 'Password must be at least 8 figuers'
+    return false
+  }
+  function passwordReError(){
+    return password !== passwordRe ? true : false
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="main-container">
+    <div className="form-container">
+      <form id="info-form" onSubmit={submit}>
+        <span>Full Name</span>
+        <input 
+            type="text" 
+            name="full-name" 
+            placeholder="e.g. Jaber Ali"
+            value={name}
+            required
+            onChange={(e) => setName(e.target.value)}
+            />
+            {nameError() && <p className="errorMessage">{nameError()}</p>}
+
+        <span>eMail</span>
+        <input 
+            type="email" 
+            name="email" 
+            placeholder="e.g. Jaber.Ali@gmail.com"
+            value={email}
+            required
+            onChange={(e) => setEmail(e.target.value)}
+            />
+            {emailError() && <p className="errorMessage">{emailError()}</p>}
+
+        <span>Password</span>
+        <input 
+            type="password" 
+            name="password" 
+            placeholder="*******"
+            value={password}
+            required
+            onChange={(e) => setPassword(e.target.value)}
+            />
+            {passwordError() && <p className="errorMessage">Plese provide a proper password </p>}
+
+        <span>Retype Password</span>
+        <input 
+            type="password" 
+            name="retypepassword" 
+            placeholder="*******"
+            value={passwordRe}
+            required
+            onChange={(e) => setPasswordRe(e.target.value)}
+            />
+            {passwordReError() && <p className="errorMessage">Passwords don't match!</p>}
+
+        
+
+        
+        <input type="submit" value="Sign Up"/>
+      </form>
+
+    </div>
+    
+  </div>
+  
     </div>
   );
 }
